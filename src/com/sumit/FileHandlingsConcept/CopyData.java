81307a4f795ff2d3636d9cddfa8543cf26afea49
Sumit Kumar Chaudhary <sumitk440@gmail.com>
2019-08-17 19:37:10 +0530

    copy data from one file to another file assignment

--- src/com/sumit/FileHandlingsConcept/CopyData.java
+++ src/com/sumit/FileHandlingsConcept/CopyData.java
@@ -8,31 +8,44 @@
 package com.sumit.FileHandlingsConcept;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CopyData 
 {
 	public static void main(String[] args) throws IOException {
 		
-		File concectFile = new File("../BasicCoceptUnderstading/File1_Copyfrom.txt");
+		File concectFile = new File("../JavaBasicConceptUderstanding/RepeatFileHandling/File1_Copyfrom.txt");
 		
 		if (concectFile.exists()) {
 		FileReader readFileCharacter = new FileReader(concectFile);
 		
 		BufferedReader appendCharacter = new BufferedReader(readFileCharacter);
 		
-		String storeData= null;
+		String storeData= "";
+		
+		File createNewFile = new File("../JavaBasicConceptUderstanding/RepeatFileHandling/File2_CopyTo.txt");
+		
+		FileWriter writeData = new FileWriter(createNewFile);
+		
+		BufferedWriter pastCopiedData = new BufferedWriter(writeData);
+		
 		
 		while ((storeData=appendCharacter.readLine())!=null)
 		{
-			storeData+=storeData;
+			pastCopiedData.write(storeData);
+			pastCopiedData.newLine();
 		}
 		
+		pastCopiedData.close();
+		System.out.println("Data successfully copied from "+concectFile.getName().toString()
+				+" to new file"+createNewFile.getName().toString());
 		
-		System.out.println(storeData);
+		
 		}
 		else 
 		{
