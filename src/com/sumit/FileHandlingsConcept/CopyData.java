c39a819d4d7d7ad85baac57f108a9247c8faca7d
Sumit Kumar Chaudhary <sumitk440@gmail.com>
2019-08-17 19:02:29 +0530

    filehandling cocept repeat

--- src/com/sumit/FileHandlingsConcept/CopyData.java
+++ src/com/sumit/FileHandlingsConcept/CopyData.java
@@ -11,20 +11,33 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class CopyData 
 {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		
-		File concectFile = new File("../BasicCoceptUnderstading/RepeatFileHandling/File1_Copyfrom.txt");
+		File concectFile = new File("../BasicCoceptUnderstading/File1_Copyfrom.txt");
 		
+		if (concectFile.exists()) {
 		FileReader readFileCharacter = new FileReader(concectFile);
 		
 		BufferedReader appendCharacter = new BufferedReader(readFileCharacter);
 		
 		String storeData= null;
 		
+		while ((storeData=appendCharacter.readLine())!=null)
+		{
+			storeData+=storeData;
+		}
 		
+		
+		System.out.println(storeData);
+		}
+		else 
+		{
+			System.out.println("File is not available on given path please check once.");
+		}
 		
 	}
 
