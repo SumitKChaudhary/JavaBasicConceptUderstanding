ed5dbf305cb4c75b5b2f3582361192f14cf6451d
Sumit Kumar Chaudhary <sumitk440@gmail.com>
2019-10-13 21:58:36 +0530

    get words length from sentences update

--- src/com/sumit/ExtraPractices/SentencesWordLength.java
+++ src/com/sumit/ExtraPractices/SentencesWordLength.java
@@ -13,25 +13,33 @@ public class SentencesWordLength
 {
 	public static void main(String[] args) 
 	{
+		//Taking scanner class for input from user 
 		Scanner takeInput = new Scanner(System.in);
 		
 		System.out.println("Enter sentences to check words length");
-		
+		//store all entered value to string variable
 		String enterSentences=takeInput.nextLine();
-		
+		//Remove space and Breaks sentences in words and store in string of array
 		String sentencesWords[]=enterSentences.split(" ");
-		
+		//taking two integer variable
 		int increaseWordCount=0, wordsLength=0;
+		
 		System.out.println("Word"+"\t:\t"+"Word's Length");
 		System.out.println("************************************************"+"\n");
+		//Getting here word by word from array and store in string variables
 		for(String word:sentencesWords)
 		{
+			//getting here length of each words 
 			for(int i=0; i<word.length(); i++)
 			{
+				//store all words length 
 				wordsLength=word.length();
+				//increase length for every words
 				wordsLength=wordsLength++;
 			}
+			//Print all data
 			System.out.println(word+"\t:\t"+wordsLength);
+			//increase the count for taking all words 
 			increaseWordCount++;
 		}
 		
