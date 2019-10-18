54db520dc5e258b79f28d4f0b0de082335e53cea
Sumit Kumar Chaudhary <sumitk440@gmail.com>
2019-10-18 13:18:40 +0530

    revers a wordof string in differenct ways

---
+++ src/com/sumit/ExtraPractices/ReverseString.java
@@ -0,0 +1,25 @@
+/**
+ * @author 			:	 sumitkumar
+ *	DATE       		:	 18-Oct-2019
+ *  FILE NAME  		: 	 ReverseString.java
+ *  PROJECT NAME 	:	 JavaBasicConceptUderstanding
+ * 
+ */
+package com.sumit.ExtraPractices;
+public class ReverseString 
+{
+	public static void main(String[] args) 
+	{
+		String str="Sumit kumar";
+		
+		char[] chars = str.toCharArray();
+		
+		for(int i=chars.length-1; i>=0; i--)
+		{
+			System.out.print(chars[i]);
+		}
+		
+	}
+	
+
+}
--- src/com/sumit/ExtraPractices/SwipeTheStringWord.java
+++ src/com/sumit/ExtraPractices/SwipeTheStringWord.java
@@ -10,18 +10,45 @@ public class SwipeTheStringWord
 {
 	public static void main(String[] args) 
 	{
-		String str = "My name is sumit";
+		String originalSentence = "My name is sumit";
+		System.out.println("Original Sendtence is \n"+originalSentence);
+		System.out.println("***** After Reverse in Different ways **********\n\n");
 		
-		String str2 ="";
-		for(int i=str.length()-1; i>=0;i--)
+		String reverseSentence ="";
+		
+//*********************************************************  METHOD 1 **********************************************************************		
+		for(int i=originalSentence.length()-1; i>=0;i--)
 		{
-			str2=str2+str.charAt(i);
-			
-			
+			reverseSentence=reverseSentence+originalSentence.charAt(i);
 		}
-		System.out.println(str2);
 		
+		System.out.println("Method 1 \t:\t"+reverseSentence);// timus si eman yM
 		
+		String words[] =originalSentence.split(" ");
+//*********************************************************  METHOD 2 **********************************************************************		
+		System.out.print("Method 2 \t:\t");
+
+		for(int i=words.length-1; i>=0; i--) 
+		{
+			System.out.print(words[i]+" ");//sumit is name My 
+		}
+		System.out.println(); 
+//*********************************************************  METHOD 3**********************************************************************		
+		System.out.print("Method 3 \t:\t");
+		
+		String reverseSentence1 ="";
+			for(String word: words)
+			{
+				String revWord ="";
+				for(int j=word.length()-1; j>=0;j--)
+				{
+					revWord = revWord+word.charAt(j);
+				}
+				
+				reverseSentence1 = reverseSentence1+revWord+" ";
+				
+			}
+		System.out.println(reverseSentence1);//yM eman si timus
 	}
 
 }
