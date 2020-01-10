003da98b6f0024ee9460355c16d694b5a7462f89
Sumit Kumar Chaudhary <sumitk440@gmail.com>
2020-01-10 15:13:27 +0530

    update project from intellij

--- src/com/sumit/StringCocept/StringRePlaceConcept.java
+++ src/com/sumit/StringCocept/StringRePlaceConcept.java
@@ -7,6 +7,7 @@
  */
 package com.sumit.StringCocept;
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class StringRePlaceConcept 
@@ -24,14 +25,25 @@ public class StringRePlaceConcept
 	{
 	
 			String actualData = "Hi, I'm {{name}}, I'm {{designation}}";
-			
-			String[] relacedData = actualData.split(" ");
-			
-			for(int i=0; i<relacedData.length; i++)
-			{
-				System.out.println(i+" "+relacedData[i]);
+
+
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the key");
+		String actualKey=null;
+
+		System.out.println("Enter the value");
+		String replaceValue=null;
+		String s=null;
+			for(int i=0; i<actualData.length(); i++) {
+
+				actualKey=sc.nextLine();
+				replaceValue=sc.nextLine();
+			s =relaceString(actualData, actualKey, replaceValue);
+
 			}
-			
+
+			System.out.println(s);
 			
 	
 	}
